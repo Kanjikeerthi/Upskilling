c39f05cfab052ad17cb07ff04b8f27cd906948fe
K <keerthi.b.k@capgemini.com>
2023-05-08 15:07:39 +0530

    my code

---
+++ src/forPractice/Reverse.java
@@ -0,0 +1,18 @@
+package forPractice;
+
+public class Reverse {
+public static void main(String[] args) {
+	/*int b = 2;
+	double d = b;
+System.out.println(d);
+  int c = 3;
+  float f = c;
+  System.out.println(c);*/
+	int a=1;
+	int b=5;
+	int d= a%b;
+	System.out.println(d);
+  
+  
+}
+}
---
+++ src/task/Pattren.java
@@ -0,0 +1,20 @@
+package task;
+
+public class Pattren {
+	
+	    public static void main(String[] args) {
+	    	 int n = 5;
+	         for (int i = 0; i < n; i++) {
+	             for (int j = 0; j < i; j++) {
+	                 System.out.print(" ");
+	             }
+	             for (int k = 0; k < (2 * n - 1) - 2 * i; k++) {
+	                 System.out.print("*");
+	             }
+	             System.out.println();
+	         }
+	    	
+	    }
+}
+	    	
+	    	
